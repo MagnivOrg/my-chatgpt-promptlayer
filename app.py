2b89ec62dfc2d217361f4cc702d530685000025a
Jared Zoneraich <jszoneraich@gmail.com>
2024-03-17 19:32:55 -0400

    while loop

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import promptlayer
+import openai
 
 load_dotenv('.env')
 promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
@@ -22,7 +23,27 @@ def main():
         pl_tags=["mychatgpt-dev"],
     )
 
-    print(response.choices[0].message.content)
+    response_message = response.choices[0].message
+
+    print(response_message.content)
+
+    messages = mychatgpt_prompt['llm_kwargs']['messages']
+    messages.append(response_message)
+    while True:
+        user_input = input("> ")
+        messages.append({"role": "user", "content": user_input})
+
+        mychatgpt_prompt['llm_kwargs']['messages'] = messages
+        response = client.chat.completions.create(
+            **mychatgpt_prompt['llm_kwargs'],
+            pl_tags=["mychatgpt-dev"],
+        )
+
+        response_message = response.choices[0].message
+
+        print(response_message.content)
+
+        messages.append(response_message)
 
 if __name__ == "__main__":
     main()
