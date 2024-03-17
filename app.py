be76faeff4d348da693921d715bfc510b0d73797
Jared Zoneraich <jszoneraich@gmail.com>
2024-03-17 19:20:11 -0400

    working!

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import os
+from dotenv import load_dotenv
+import promptlayer
+
+load_dotenv('.env')
+promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
+
+OpenAI = promptlayer.openai.OpenAI
+client = OpenAI()
 
 def main():
-    # Your main code goes here
-    print("Hello, World!")
+    user_input = input("Welcome to MyChatGPT! How can I help?\n> ")
+    mychatgpt_prompt = promptlayer.templates.get("MyChatGPT", {
+        "provider": "openai",
+        "input_variables": {
+            "question": user_input
+        }
+    })
+
+    response = client.chat.completions.create(
+        **mychatgpt_prompt['llm_kwargs'],
+        pl_tags=["mychatgpt-dev"],
+    )
+
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
