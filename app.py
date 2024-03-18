3b5ff065fa350045cbe82574aff79069c95cc569
Jared Zoneraich <jszoneraich@gmail.com>
2024-03-18 19:58:06 -0400

    function calling

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import promptlayer
 import openai
+import json
+import datetime
 
 load_dotenv('.env')
 promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
@@ -9,12 +11,36 @@ promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
 OpenAI = promptlayer.openai.OpenAI
 client = OpenAI()
 
+today_date = datetime.datetime.now().strftime("%Y-%m-%d")
+location = "New York City"
+
+def parse_llm_response(response_message):
+    if response_message.content is not None:
+        print(response_message.content)
+        return None
+    if response_message.function_call is not None:
+        if response_message.function_call.name == "calculator":
+            parsed_args = json.loads(response_message.function_call.arguments)
+            evaluated = eval(parsed_args['expression'])
+            print("$ ", evaluated)
+            return {
+                "role": "function",
+                "content": str(evaluated),
+                "name": "calculator",
+            }
+        else:
+            print("Function call not supported")
+    return None
+            
+
 def main():
     user_input = input("Welcome to MyChatGPT! How can I help?\n> ")
     mychatgpt_prompt = promptlayer.templates.get("MyChatGPT", {
         "provider": "openai",
         "input_variables": {
-            "question": user_input
+            "question": user_input,
+            "date": today_date,
+            "location": location,
         }
     })
 
@@ -24,11 +50,13 @@ def main():
     )
 
     response_message = response.choices[0].message
-
-    print(response_message.content)
-
     messages = mychatgpt_prompt['llm_kwargs']['messages']
     messages.append(response_message)
+
+    parsed_message = parse_llm_response(response_message)
+    if parsed_message is not None:
+        messages.append(parsed_message)
+
     while True:
         user_input = input("> ")
         messages.append({"role": "user", "content": user_input})
@@ -40,10 +68,11 @@ def main():
         )
 
         response_message = response.choices[0].message
-
-        print(response_message.content)
-
         messages.append(response_message)
+
+        parsed_message = parse_llm_response(response_message)
+        if parsed_message is not None:
+            messages.append(parsed_message)
 
 if __name__ == "__main__":
     main()
